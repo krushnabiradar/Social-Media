8be91a0fe09070728213c9e7b3d2233aace24881
krushnabiradar <krushnabiradar1995@.com>
2022-08-24 09:33:26 +0530

    first commit

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,4 @@
+export const BASE_PROFILE_IMAGE_URL='http://localhost:4000/public/uploads/profiles/';
+export const BASE_POST_IMAGE_URL='http://localhost:4000/public/uploads/posts/';
+
+export const SOCKET_ENDPOINT = "http://localhost:4000";
